e8fb9c84f452e782ca5e8ba0ca94c8406eceb811
jwaes <jaco.waes@gmail.com>
2024-06-13 09:40:47 +0000

    branched

--- __manifest__.py
+++ __manifest__.py
@@ -15,10 +15,10 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.5',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','website_sale'],
+    'depends': ['base', 'website_sale'],
 
     # always loaded
     'data': [
